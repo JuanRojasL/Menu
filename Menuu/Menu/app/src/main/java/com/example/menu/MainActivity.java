96ea9697cf9ba6e8159b245af6f1b9a5141b834d
JuanRojasL <112908543+JuanRojasL@users.noreply.github.com>
2022-10-28 12:40:43 -0500

    Add files via upload

---
+++ Menuu/Menu/app/src/main/java/com/example/menu/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.item1) {
+            Toast.makeText(this, "Bienvenidos a este proyecto", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.item2) {
+            Toast.makeText(this, "Bienvenidos a este proyecto", Toast.LENGTH_SHORT).show();
+
+        } else if (id == R.id.item3) {
+            Toast.makeText(this, "Bienvenidos a este proyecto", Toast.LENGTH_SHORT).show();
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
